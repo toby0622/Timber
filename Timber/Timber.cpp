2a0934bb44ec26794d03cb571fa8e3aee1e7a43c
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2022-08-01 16:54:56 +0800

    Fundamental Setup

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -1,5 +1,33 @@
-﻿// Where The Game Start
+﻿// SFML Libraries
+#include <SFML/Graphics.hpp>
+// Using Namespace "sf"
+using namespace sf;
 
 int main() {
+	// VideoMode Object Create
+	VideoMode vm(1920, 1080);
+
+	// Game Window Create
+	RenderWindow window(vm, "Timber!!! Imitation", Style::Fullscreen);
+
+	while (window.isOpen()) {
+		// Player Input Handle
+		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+			window.close();
+		}
+		
+		// Scene Update
+
+		// Scene Draw
+
+		// Last Frame Clear
+		window.clear();
+
+		// Scene Draw
+		
+		// Game Display
+		window.display();
+	}
+
 	return 0;
 }
